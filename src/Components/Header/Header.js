b1d730016654f352a981e96340013f853b56d96a
sabbir322 <sabbirhosan370@gmail.com>
2022-06-10 16:39:03 +0600

    my projects added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 import { AiOutlineMenuUnfold, AiOutlineClose } from 'react-icons/ai';
 
@@ -12,14 +12,15 @@ const Header = () => {
         </div>
         <div>
           <nav className={openNav ? " nav-close":"nav-open"}>
-            {/* <NavLink className="link-btn" to="/">Home</NavLink>
-            <NavLink className="link-btn" to="/blogs">Blogs</NavLink> */}
-            <a className='link-btn' href="#home">Home</a>
-            <a className='link-btn' href="#skils">Skils</a>
-            <a className='link-btn' href="#about">About</a>
-            <a className='link-btn' href="#project">Project</a>
-            <a className='link-btn' href="#contact">Contact</a>
-            <a className='link-btn' href="#blog">Blog</a>
+            <NavLink className="link-btn" to="/">Home</NavLink>
+            <NavLink className="link-btn" to="/blogs">Blogs</NavLink> 
+            {/* <a className='link-btn' href="/">Home</a>
+            <a className='link-btn' href="/skils">Skils</a>
+            <a className='link-btn' href="/about">About</a>
+            <a className='link-btn' href="/project">Project</a>
+            <a className='link-btn' href="/contact">Contact</a> */}
+            {/* <a className='link-btn' href="#blog">Blog</a> */}
+            {/* <NavLink className='link-btn' to="/blog">Blog</NavLink> */}
           </nav>
           <button onClick={() => {setOpenNav(!openNav)}} className="menu-ico">{openNav ? <AiOutlineClose></AiOutlineClose> : <AiOutlineMenuUnfold></AiOutlineMenuUnfold>}</button>
         </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import './Projects.css';
+import React from "react";
+import "./Projects.css";
+import project1 from "../../images/project-1.jpg";
+import project2 from "../../images/project-2.jpg";
+import project3 from "../../images/project-3.jpg";
 
 const Projects = () => {
-    return (
-        <div className='projects'>
-            <h1 className='text-center fw-semibold'>MY PROJECTS</h1>
-            <hr />
+  return (
+    <div className="projects">
+      <h1 className="text-center fw-semibold">MY PROJECTS</h1>
+      <hr />
+      <div className="projects-container">
+        <div className="project">
+          <img src={project3} alt="" />
+          <h4 className="fw-bold text-center my-3">Pro Tools</h4>
+        <div className="d-flex justify-content-center px-2 mb-2">
+            <a className="btn btn-primary" target={"_blank"} href="https://manufacturer-30951.web.app/">Live Site</a>
+            <button className="btn btn-primary mx-4">Details</button>
+            <a className="btn btn-primary" target={"_blank"} href="https://github.com/sabbir4212/warehouse-management-client-side">Github</a>
         </div>
-    );
+        </div>
+        <div className="project">
+          <img src={project2} alt="" />
+          <h4 className="fw-bold text-center my-3">Sheos House</h4>
+          <div className="d-flex justify-content-center px-2 mb-2">
+            <a className="btn btn-primary" target={"_blank"} href="https://warehouse-68b1a.web.app/">Live Site</a>
+            <button className="btn btn-primary mx-4">Details</button>
+            <a className="btn btn-primary" target={"_blank"} href="https://github.com/sabbir4212/shoes-house-management-client-side">Github</a>
+        </div>
+        </div>
+        <div className="project">
+          <img src={project1} alt="" />
+          <h4 className="fw-bold text-center my-3">The Bodycoach</h4>
+          <div className="d-flex justify-content-center px-2 mb-2">
+            <a className="btn btn-primary" target={"_blank"} href="https://the-bodycoach-cf175.web.app/">Live Site</a>
+            <button className="btn btn-primary mx-4">Details</button>
+            <a className="btn btn-primary" target={"_blank"} href="https://github.com/sabbir4212/independent-service-provider">Github</a>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
