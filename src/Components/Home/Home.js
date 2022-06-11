2f072b1c93d7c9168315da14882399ca44b48bab
sabbir322 <sabbirhosan370@gmail.com>
2022-06-11 14:16:33 +0600

    my skil done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,12 +4,14 @@ import Banner from '../Banner/Banner';
 import Blogs from '../Blogs/Blogs';
 import ContactMe from '../ContactMe/ContactMe';
 import Projects from '../Projects/Projects';
+import Skils from '../Skils/Skils';
 import './Home.css';
 
 const Home = () => {
     return (
         <div className='home'>
             <Banner></Banner>
+            <Skils></Skils>
             <About></About>
             <Projects></Projects>
             <ContactMe></ContactMe>
